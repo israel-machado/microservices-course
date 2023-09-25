70c52999a10638cb4037e5758e93f7b35ba8949f
Israel Machado <israelmach26@gmail.com>
2023-09-25 10:33:36 -0300

    Random port to hr-worker

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS tb_worker (
+                                         id INT AUTO_INCREMENT PRIMARY KEY,
+                                         name VARCHAR(255) NOT NULL,
+    daily_income DECIMAL(10, 2) NOT NULL
+    );
+
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
